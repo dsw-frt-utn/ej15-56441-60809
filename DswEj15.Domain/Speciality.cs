35db3deab05943fc7ac157ed9649c366959c5adc
Martina Gomez <martina.gomez139@gmail.com>
2026-06-17 19:12:04 -0300

    Subo solucion hasta el apartado 3.e, inclusive.

---
+++ DswEj15.Domain/Speciality.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej15.Domain
+{
+    public class Speciality : BaseEntity
+    {
+        public required String Name { get; set; }
+        public String? Description { get; set; }
+    }
+}
