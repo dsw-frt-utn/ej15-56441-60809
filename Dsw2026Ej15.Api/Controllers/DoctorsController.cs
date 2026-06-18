9210e229e8ace6754254a05168949c76d8a3160f
Guadalupe Chaya <chayaguada22@gmail.com>
2026-06-18 02:55:13 -0300

    Resuelto hasta el apartado 3g Primer endpoint

---
+++ Dsw2026Ej15.Api/Controllers/DoctorsController.cs
@@ -0,0 +1,38 @@
+﻿using Dsw2026Ej15.Api.Models;
+using Dsw2026Ej15.Domain.Entities;
+using Dsw2026Ej15.Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dsw2026Ej15.Api.Controllers;
+
+    [ApiController]
+    [Route("api/doctors")]
+    public class DoctorsController : ControllerBase
+    {
+    private readonly IPersistence _persistence;
+
+    public DoctorsController(IPersistence persistence)
+    {
+        _persistence = persistence;
+    }
+
+
+        [HttpPost]
+        public async Task<IActionResult> CreateDoctor(DoctorModel.Request request)
+        {  
+           if(string.IsNullOrWhiteSpace(request.Name)|| string.IsNullOrWhiteSpace(request.LicenseNumber))
+        {
+            return BadRequest("Nombre y matricula son requeridos");
+        }
+
+        var speciality = _persistence.GetSpecialityById(request.SpecialityId);
+        if(speciality == null) 
+        {
+            return BadRequest("Especialidad no existe");
+        }
+
+        var doctor = new Doctor(request.Name, request.LicenseNumber, speciality);
+        _persistence.SaveDoctor(doctor);
+        return Created();
+    }
+}
---
+++ DswEj15.Domain/Interfaces/IPersistence.cs
@@ -0,0 +1,13 @@
+﻿using Dsw2026Ej15.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej15.Domain.Interfaces
+{
+    public interface IPersistence
+    {
+        Speciality? GetSpecialityById(Guid id);
+        void SaveDoctor(Doctor doctor);
+    }
+}
